1449ad6b589511fd22227917f1436eda2cfc4346
Little-Bear-04 <142242630+Little-Bear-04@users.noreply.github.com>
2023-10-28 08:30:25 +0100

    Update SignIn.js
    Updated fetch link

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -17,7 +17,7 @@ class SignIn  extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('https://facial-recognition-back-end.onrender.com', {
+        fetch('https://facial-recognition-back-end.onrender.com/signin', {
             method: 'post',
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify ({
