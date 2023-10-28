a6c381d7cfc77621901938b78aacf0b825e29797
Little-Bear-04 <142242630+Little-Bear-04@users.noreply.github.com>
2023-10-28 08:29:48 +0100

    Update Register.js
    Updated fetch link

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('https://facial-recognition-back-end.onrender.com', {
+        fetch('https://facial-recognition-back-end.onrender.com/register', {
             method: 'post',
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify ({
